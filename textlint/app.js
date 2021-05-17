2a9e6e57baefa9633009880ccfa8ab8c83b36442
tomoyk <tmyk.kym@gmail.com>
2021-05-17 11:35:17 +0900

    add app.js

---
+++ textlint/app.js
@@ -0,0 +1,62 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const router = (req, res, httpVersion, method, path) => {
+    console.log("%s %s  HTTP/%d", method, path, httpVersion);
+    
+    if (method === "GET" && path === "/") {
+
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+        });
+        const resBody = {httpVersion, method, path};
+        res.write(JSON.stringify(resBody));
+        res.end();
+
+    } else if (method === "POST" && path === "/") {
+
+        let body = [];
+        req.on('error', (err) => {  // Error Handling
+
+            console.error(err);
+
+        }).on('data', (chunk) => {  // Append Payload
+
+            body.push(chunk);
+            console.log(body);
+
+        }).on('end', () => {  // Make Response
+
+            // Concat Body
+            body = Buffer.concat(body).toString();
+            res.on('error', (err) => {
+                console.error(err);
+            });
+
+            // Create Response
+            res.writeHead(200, {
+                'Content-Type': 'application/json',
+            });
+            const resBody = {httpVersion, method, path, body};
+            res.write(JSON.stringify(resBody));
+            res.end();
+
+        });
+        
+
+        
+        
+
+    }
+}
+
+const server = http.createServer((request, response) => {
+    const { httpVersion, headers, url, method } = request;
+    router(request, response, httpVersion, method, url);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
